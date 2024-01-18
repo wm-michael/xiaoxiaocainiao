6c49f31b19c6ecd5451d0c0ae8e7b275b144344b
wm-michael <wm.michaelwang@gmail.com>
2024-01-18 11:46:28 +0800

    添加Compose Preview

--- module-compose/src/main/java/com/example/module_compose/ComposeMainActivity.kt
+++ module-compose/src/main/java/com/example/module_compose/ComposeMainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 class ComposeMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,4 +18,13 @@ class ComposeMainActivity : AppCompatActivity() {
             Text("Hello world!", modifier = Modifier.background(color = Color.White))
         }
     }
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposePreview() {
+    Text("Hello world!", modifier = Modifier.background(color = Color.White))
 }
